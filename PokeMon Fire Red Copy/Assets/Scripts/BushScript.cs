85ebbd12d4a828f51aacd81a072286ec227de21c
sam <swzwij@gmail.com>
2021-12-02 14:31:04 +0100

    added bush

--- PokeMon Fire Red Copy/Assets/Scripts/BushScript.cs
+++ PokeMon Fire Red Copy/Assets/Scripts/BushScript.cs
@@ -6,25 +6,31 @@ public class BushScript : MonoBehaviour
 {
     [SerializeField] private GameObject _bush1;
     [SerializeField] private GameObject _bush2;
-    //[SerializeField] private GameObject _Leave1;
-    //[SerializeField] private GameObject _Leave2;
-    //[SerializeField] private ParticleSystem _leaves;
+
+    [SerializeField] private GameObject _bushTop1;
+
+    [SerializeField] private GameObject _leaves1;
+    [SerializeField] private GameObject _leaves2;
 
     private bool isEncounter;
 
     private void Start()
     {
 
-        _bush2.SetActive(false);
         _bush1.SetActive(true);
-        //_Leave1.SetActive(false);
-        //_Leave2.SetActive(false);
+        _bush2.SetActive(false);
+        _bushTop1.SetActive(false);
+        _leaves1.SetActive(false);
+        _leaves2.SetActive(false);
+    }
+
+    private void Update()
+    {
+        
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _bush1.SetActive(true);
-        _bush2.SetActive(false);
 
         int encounter = Random.Range(1, 16);
 
@@ -36,30 +42,43 @@ public class BushScript : MonoBehaviour
             print("Encounter");
         }
 
-        //StartCoroutine(BushEnter());
+        StartCoroutine(BushEnter());
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _bush1.SetActive(false);
-        _bush2.SetActive(true);
+        _bushTop1.SetActive(false);
     }
 
-    /*IEnumerator BushEnter()
+    IEnumerator BushEnter()
     {
-        
+        _bush1.SetActive(false);
+        _bush2.SetActive(true);
 
         yield return new WaitForSeconds(0.2f);
-        _Leave1.SetActive(true);
-        _Leave2.SetActive(false);
+
+        StartCoroutine(Leaves());
+
+        _bush1.SetActive(true);
+        _bush2.SetActive(false);
+        _bushTop1.SetActive(true);
+    }
+
+    IEnumerator Leaves()
+    {
+
+        _leaves1.SetActive(true);
+        _leaves2.SetActive(false);
 
         yield return new WaitForSeconds(0.2f);
-        _Leave1.SetActive(false);
-        _Leave2.SetActive(true);
+
+        _leaves1.SetActive(false);
+        _leaves2.SetActive(true);
 
         yield return new WaitForSeconds(0.2f);
-        _Leave1.SetActive(false);
-        _Leave2.SetActive(false);
-    }*/
+
+        _leaves1.SetActive(false);
+        _leaves2.SetActive(false);
+    }
 
 }
